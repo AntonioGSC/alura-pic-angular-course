b09ec67ca568f66f7903f0a329297772560ee9ec
tony <antoniogabrielagsc@gmail.com>
2022-05-18 20:59:44 -0300

    Resolver

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 import { Photo } from '../photo/photo.model';
 import { PhotoService } from '../photo/photo.service';
 
@@ -13,16 +14,14 @@ export class PhotoListComponent implements OnInit {
     photos: Photo[] = [];
     filter: string = '';
 
-    constructor(
-        private photoService: PhotoService,
-        private activatedRoute: ActivatedRoute
-    ){}
+    constructor(private activatedRoute: ActivatedRoute){}
 
     ngOnInit(): void{
-        const userNameParam = this.activatedRoute.snapshot.params['userName'];
-        this.photoService.listFromUser(userNameParam)
-        .subscribe(photos => {
-            this.photos = photos;
-        }, err => console.log(err));
+        this.photos = this.activatedRoute.snapshot.data['photos'];
+        // const userNameParam = this.activatedRoute.snapshot.params['userName'];
+        // this.photoService.listFromUser(userNameParam)
+        // .subscribe(photos => {
+        //     this.photos = photos;
+        // }, err => console.log(err));
     }
 }
